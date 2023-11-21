58f82469dfc961895b546454c812959744a00adc
dafix.041 <vcruzz1977@gmail.com>
2023-11-20 21:45:12 -0300

    primeiro commit

---
+++ Migrations/20231120233821_StatusAdd.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class StatusAdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Tarefas",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "CategoriaId",
+                keyValue: 1,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 21, 20, 38, 21, 141, DateTimeKind.Local).AddTicks(2896));
+
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "CategoriaId",
+                keyValue: 2,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 22, 20, 38, 21, 141, DateTimeKind.Local).AddTicks(2903));
+
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "CategoriaId",
+                keyValue: 3,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 23, 20, 38, 21, 141, DateTimeKind.Local).AddTicks(2905));
+
+            migrationBuilder.UpdateData(
+                table: "Tarefas",
+                keyColumn: "TarefaId",
+                keyValue: 1,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 27, 20, 38, 21, 141, DateTimeKind.Local).AddTicks(2975));
+
+            migrationBuilder.UpdateData(
+                table: "Tarefas",
+                keyColumn: "TarefaId",
+                keyValue: 2,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 23, 20, 38, 21, 141, DateTimeKind.Local).AddTicks(2976));
+
+            migrationBuilder.UpdateData(
+                table: "Tarefas",
+                keyColumn: "TarefaId",
+                keyValue: 3,
+                column: "CriadoEm",
+                value: new DateTime(2023, 12, 4, 20, 38, 21, 141, DateTimeKind.Local).AddTicks(2977));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Tarefas");
+
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "CategoriaId",
+                keyValue: 1,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 21, 20, 32, 16, 563, DateTimeKind.Local).AddTicks(1236));
+
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "CategoriaId",
+                keyValue: 2,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 22, 20, 32, 16, 563, DateTimeKind.Local).AddTicks(1242));
+
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "CategoriaId",
+                keyValue: 3,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 23, 20, 32, 16, 563, DateTimeKind.Local).AddTicks(1243));
+
+            migrationBuilder.UpdateData(
+                table: "Tarefas",
+                keyColumn: "TarefaId",
+                keyValue: 1,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 27, 20, 32, 16, 563, DateTimeKind.Local).AddTicks(1326));
+
+            migrationBuilder.UpdateData(
+                table: "Tarefas",
+                keyColumn: "TarefaId",
+                keyValue: 2,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 23, 20, 32, 16, 563, DateTimeKind.Local).AddTicks(1328));
+
+            migrationBuilder.UpdateData(
+                table: "Tarefas",
+                keyColumn: "TarefaId",
+                keyValue: 3,
+                column: "CriadoEm",
+                value: new DateTime(2023, 12, 4, 20, 32, 16, 563, DateTimeKind.Local).AddTicks(1329));
+        }
+    }
+}
---
+++ Migrations/20231121002034_DBStatus.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class DBStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Tarefas");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "Tarefas",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Status",
+                columns: table => new
+                {
+                    StatusId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StatusDescricao = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Status", x => x.StatusId);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "CategoriaId",
+                keyValue: 1,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 21, 21, 20, 34, 239, DateTimeKind.Local).AddTicks(9263));
+
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "CategoriaId",
+                keyValue: 2,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 22, 21, 20, 34, 239, DateTimeKind.Local).AddTicks(9269));
+
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "CategoriaId",
+                keyValue: 3,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 23, 21, 20, 34, 239, DateTimeKind.Local).AddTicks(9270));
+
+            migrationBuilder.UpdateData(
+                table: "Tarefas",
+                keyColumn: "TarefaId",
+                keyValue: 1,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 27, 21, 20, 34, 239, DateTimeKind.Local).AddTicks(9343));
+
+            migrationBuilder.UpdateData(
+                table: "Tarefas",
+                keyColumn: "TarefaId",
+                keyValue: 2,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 23, 21, 20, 34, 239, DateTimeKind.Local).AddTicks(9346));
+
+            migrationBuilder.UpdateData(
+                table: "Tarefas",
+                keyColumn: "TarefaId",
+                keyValue: 3,
+                column: "CriadoEm",
+                value: new DateTime(2023, 12, 4, 21, 20, 34, 239, DateTimeKind.Local).AddTicks(9347));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tarefas_StatusId",
+                table: "Tarefas",
+                column: "StatusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tarefas_Status_StatusId",
+                table: "Tarefas",
+                column: "StatusId",
+                principalTable: "Status",
+                principalColumn: "StatusId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tarefas_Status_StatusId",
+                table: "Tarefas");
+
+            migrationBuilder.DropTable(
+                name: "Status");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tarefas_StatusId",
+                table: "Tarefas");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "Tarefas");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Tarefas",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "CategoriaId",
+                keyValue: 1,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 21, 20, 38, 21, 141, DateTimeKind.Local).AddTicks(2896));
+
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "CategoriaId",
+                keyValue: 2,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 22, 20, 38, 21, 141, DateTimeKind.Local).AddTicks(2903));
+
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "CategoriaId",
+                keyValue: 3,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 23, 20, 38, 21, 141, DateTimeKind.Local).AddTicks(2905));
+
+            migrationBuilder.UpdateData(
+                table: "Tarefas",
+                keyColumn: "TarefaId",
+                keyValue: 1,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 27, 20, 38, 21, 141, DateTimeKind.Local).AddTicks(2975));
+
+            migrationBuilder.UpdateData(
+                table: "Tarefas",
+                keyColumn: "TarefaId",
+                keyValue: 2,
+                column: "CriadoEm",
+                value: new DateTime(2023, 11, 23, 20, 38, 21, 141, DateTimeKind.Local).AddTicks(2976));
+
+            migrationBuilder.UpdateData(
+                table: "Tarefas",
+                keyColumn: "TarefaId",
+                keyValue: 3,
+                column: "CriadoEm",
+                value: new DateTime(2023, 12, 4, 20, 38, 21, 141, DateTimeKind.Local).AddTicks(2977));
+        }
+    }
+}
